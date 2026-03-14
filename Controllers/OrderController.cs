474ed25ba5e1207670a5e42f4102b02f2cce3cdd
HoaiPhuc2505 <2324802010322@student.tdmu.edu.vn>
2026-03-14 10:57:23 +0700

    Add project files.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Messaging.Common.Publishing;
+
+namespace ECommerceSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateOrder()
+        {
+            var publisher = new MessagePublisher();
+
+            publisher.Publish("New order created!");
+
+            return Ok("Message sent to RabbitMQ");
+        }
+    }
+}
